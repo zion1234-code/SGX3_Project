df5229c174b67fc099a74e977d4f981fe574062c
Zion Pease <zionp2002@gmail.com>
2025-06-18 18:19:29 +0000

    Update app.py with Flask routes

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
+import pandas as pd
+import os
 
+# Define your global DataFrame
+traffic_df = None
 
 app = Flask(__name__)
 
-#More line of code
-#More Line of code
+def load_traffic_data():
+    global traffic_df
+    print("Loading Austin Traffic Data...")
+    traffic_df = pd.read_csv("atxtraffic.csv")
+    print(f"Loaded {len(traffic_df)} rows into memory.")
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return "Hello World!\n"
-@app.route('/<name>',methods=['GET'])
-def hello_name(name):
-    return f'Hello,{name}\n'
-#the last line of you flask application
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8046)
+@app.route("/")
+def index():
+    global traffic_df
+    sample = traffic_df.head(10).to_dict(orient="records")
+    return jsonify(sample)
 
+@app.route("/")
+def top():
+    global traffic_df
+    num = request.args.get(int('count'))
+    sample = traffic_df.head(num).to_dict(orient="records")
+    return jsonify(sample)
 
+if __name__ == "__main__":
+    load_traffic_data()  # <- This runs BEFORE the server starts
+    app.run(debug=True, host="0.0.0.0", port=8046)
